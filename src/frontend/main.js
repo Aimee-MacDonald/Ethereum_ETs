093af372dd9496d7b59f0646805d93fec0fbd865
Aimee MacDonald <aimeelmacdonald@gmail.com>
2022-01-25 20:22:47 +0200

    Project Setup

---
+++ src/frontend/main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'normalize.css'
+import './main.sass'
+
+const Main = () => (
+  <div id='Main'>
+    <h1>MAIN</h1>
+  </div>
+)
+
+ReactDOM.render(<Main/>, document.getElementById('root'))
